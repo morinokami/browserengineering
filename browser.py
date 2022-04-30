99c0bbec35eaca314381ca411c9fc8aeec3bf182
Shinya Fujino <shf0811@gmail.com>
2022-04-30 21:58:15 +0900

    Chapter 1

--- browser.py
+++ browser.py
@@ -1,2 +1,83 @@
-number = input("What is your favourite number?")
-print("It is", number + 1)
+import socket
+import ssl
+from typing import Dict, Tuple
+
+
+Headers = Dict[str, str]
+Body = str
+
+
+def request(url: str) -> Tuple[Headers, Body]:
+    scheme, url = url.split("://", 1)
+    assert scheme in ["http", "https"], "Unknown scheme {}".format(scheme)
+
+    if "/" in url:
+        host, path = url.split("/", 1)
+        path = "/" + path
+    else:
+        host = url
+        path = "/"
+    port = 80 if scheme == "http" else 443
+
+    if ":" in host:
+        host, _port = host.split(":", 1)
+        port = int(_port)
+
+    s = socket.socket(
+        family=socket.AF_INET,
+        type=socket.SOCK_STREAM,
+        proto=socket.IPPROTO_TCP,
+    )
+    s.connect((host, port))
+
+    if scheme == "https":
+        ctx = ssl.create_default_context()
+        s = ctx.wrap_socket(s, server_hostname=host)
+
+    s.send(
+        "GET {} HTTP/1.0\r\n".format(path).encode("utf8")
+        + "Host: {}\r\n\r\n".format(host).encode("utf8")
+    )
+    response = s.makefile("r", encoding="utf8", newline="\r\n")
+
+    statusline = response.readline()
+    version, status, explanation = statusline.split(" ", 2)
+    assert status == "200", "{}: {}".format(status, explanation)
+
+    headers: Headers = {}
+    while True:
+        line = response.readline()
+        if line == "\r\n":
+            break
+        header, value = line.split(":", 1)
+        headers[header.lower()] = value.strip()
+
+    assert "transfer-encoding" not in headers
+    assert "content-encoding" not in headers
+
+    body = response.read()
+    s.close()
+
+    return headers, body
+
+
+def show(body: Body) -> None:
+    in_angle = False
+    for c in body:
+        if c == "<":
+            in_angle = True
+        elif c == ">":
+            in_angle = False
+        elif not in_angle:
+            print(c, end="")
+
+
+def load(url: str) -> None:
+    headers, body = request(url)
+    show(body)
+
+
+if __name__ == "__main__":
+    import sys
+
+    load(sys.argv[1])
